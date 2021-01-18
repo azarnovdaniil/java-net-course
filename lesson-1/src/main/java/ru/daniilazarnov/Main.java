58a702587124aee268ce99875f1412ca11234c6f
daniilazarnov <azarnow.d@gmail.com>
2021-01-18 20:17:47 +0300

    Added presentation for lesson 1

--- lesson-1/src/main/java/ru/daniilazarnov/Main.java
+++ lesson-1/src/main/java/ru/daniilazarnov/Main.java
@@ -3,7 +3,6 @@ package ru.daniilazarnov;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println();
-        //[1][2][3][4] - meta [][][][][] - file
+        System.out.println("Lesson 1");
     }
 }
