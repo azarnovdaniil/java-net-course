d806d3de3270fcda9b82e3ae013ac904baa0b9e1
Aleks <alex_sotnikov89@mail.ru>
2021-01-28 17:58:56 +0300

    Squash

--- project/client/src/main/java/ru/daniilazarnov/Client.java
+++ project/client/src/main/java/ru/daniilazarnov/Client.java
@@ -1,8 +1,14 @@
 package ru.daniilazarnov;
 
+import java.util.concurrent.CountDownLatch;
+
+
 public class Client {
 
-    public static void main(String[] args) {
-        System.out.println("Client!");
+    public static void main(String[] args) throws Exception {
+        CountDownLatch networkStarter = new CountDownLatch(1);
+        new Thread(() -> Network.getInstance().start(networkStarter)).start();
+        networkStarter.await();
+
     }
 }
--- project/server/src/main/java/ru/daniilazarnov/Server.java
+++ project/server/src/main/java/ru/daniilazarnov/Server.java
@@ -1,8 +1,36 @@
 package ru.daniilazarnov;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
 public class Server {
 
     public static void main(String[] args) {
-        System.out.println("Server!");
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap sb = new ServerBootstrap();
+            sb.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(new ServerHandler());
+                        }
+                    });
+            ChannelFuture future = sb.bind(8189).sync();
+            future.channel().closeFuture().sync();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
     }
 }
+
