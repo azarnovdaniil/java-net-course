7811ba32cf0249532448c0773127c8cd999a73cc
Valeriy Lazarev <v.lazarev.spb@gmail.com>
2021-01-30 12:46:55 +0300

    work on client and server

--- project/client/src/main/java/ru/daniilazarnov/Client.java
+++ project/client/src/main/java/ru/daniilazarnov/Client.java
@@ -1,8 +1,98 @@
 package ru.daniilazarnov;
 
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
+import java.net.ConnectException;
+import java.net.InetSocketAddress;
+import java.nio.Buffer;
+import java.nio.ByteBuffer;
+import java.nio.channels.AlreadyConnectedException;
+import java.nio.channels.AsynchronousCloseException;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+
 public class Client {
 
-    public static void main(String[] args) {
-        System.out.println("Client!");
+    /*
+            [byte  ] 1b управляющий байт →
+            [short ] 2b длинна имени файла →
+            [byte[]] nb  имя файла →
+            [long  ] 8b размер файла →
+            [byte[]] nb содержимое файла
+     */
+    private static final Logger log = Logger.getLogger(Client.class);
+
+
+    //    private static SocketChannel client;
+    private final Selector selector;
+    private static SocketChannel client;
+
+    public Client(Selector selector) {
+        this.selector = selector;
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        client = SocketChannel.open();
+        String msg = "Hello\n";
+        String hostname = "localhost";
+        int port = 8189;
+        connect(hostname, port);
+        ByteBuffer buffer;
+        buffer = sendMessage(client, msg);
+        readingInMessage(client, buffer);
+
+        System.out.println();
+        while (client.isOpen()) {
+
+        }
+    }
+
+    private static void connect(String hostname, int port) throws IOException {
+        InetSocketAddress serverAddress = new InetSocketAddress(hostname,
+                port);
+
+
+        /*В этом цикле ждем запуск сервера*/
+        while (!client.isConnected()) {
+            client = SocketChannel.open();
+            try {
+                client.connect(serverAddress);
+            } catch (ConnectException | AsynchronousCloseException e) {
+                log.info("Ожидание сервера");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException interruptedException) {
+                    log.error(interruptedException);
+                }
+            }
+
+        }
+
+//        client.connect(serverAddress);
+//        log.debug();
+        log.info("Соединение с сервером установлено");
+    }
+
+    //    @org.jetbrains.annotations.NotNull
+    private static ByteBuffer sendMessage(SocketChannel client, String msg) throws IOException {
+        ByteBuffer buffer = ByteBuffer.wrap(msg.getBytes());
+        log.debug(buffer);
+        client.write(buffer);
+        return buffer;
+    }
+
+    private static void readingInMessage(SocketChannel client, ByteBuffer buffer) throws IOException {
+        int i = client.read(buffer);
+        while (i != -1) {
+            buffer.flip(); //Переключение буфера в режим чтения
+
+            while (buffer.hasRemaining()) {
+                System.out.print((char) buffer.get()); // побайтово получаем содержимое буфера
+            }
+            buffer.clear();
+            i = client.read(buffer); //получаем следующий фрагмент
+        }
     }
 }
--- project/server/src/main/java/ru/daniilazarnov/Server.java
+++ project/server/src/main/java/ru/daniilazarnov/Server.java
@@ -1,8 +1,117 @@
 package ru.daniilazarnov;
 
-public class Server {
+import org.apache.log4j.Logger;
 
-    public static void main(String[] args) {
-        System.out.println("Server!");
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
+import java.util.Iterator;
+
+public class Server implements Runnable {
+    private static final Logger log = Logger.getLogger(Server.class);
+    private final ServerSocketChannel serverSocketChannel;
+    private final Selector selector; // слушает все события
+    private final ByteBuffer buf = ByteBuffer.allocate(256);
+    private int acceptedClientIndex = 1;
+    private final ByteBuffer welcomeBuf = ByteBuffer.wrap("Welcome to chat!\n".getBytes());
+
+    Server() throws IOException {
+        this.serverSocketChannel = ServerSocketChannel.open(); //создаем ССЧ
+        this.serverSocketChannel.socket().bind(new InetSocketAddress(8189)); //устанавливаем порт для ССЧ, который он будет слушать
+        this.serverSocketChannel.configureBlocking(false);
+        this.selector = Selector.open();
+        this.serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT); // регистрация селектора на канналше с определенным ключом
+    }
+
+    public static void main(String[] args) throws IOException {
+        new Thread(new Server()).start();
+    }
+
+    @Override
+    public void run() {
+        try {
+            log.info("Сервер запущен (Порт: 8189)");
+            Iterator<SelectionKey> iter;
+            SelectionKey key;
+            while (this.serverSocketChannel.isOpen()) { // внутри вайла канал открыт
+                selector.select();// ждем новых ключей
+                iter = this.selector.selectedKeys().iterator();
+                while (iter.hasNext()) { // перебираем ключи в итераторе
+                    key = iter.next();
+                    iter.remove();
+                    if (key.isAcceptable()) { // проверяем ключ
+                        handleAccept(key);
+                    }
+                    if (key.isReadable()) {
+                        handleRead(key);
+                    }
+                }
+            }
+        } catch (IOException e) {
+//            e.printStackTrace();
+            log.error(e.getLocalizedMessage());
+        }
+    }
+
+    /**
+     * Метод подключает новых клиентов
+     * @param key Ключ выбора создается каждый раз, когда канал регистрируется с помощью селектора.
+     *            Ключ остается действительным до тех пор, пока он не будет отменен вызовом его метода cancel
+     * @throws IOException исключение
+     */
+    private void handleAccept(SelectionKey key) throws IOException {
+        SocketChannel sc = ((ServerSocketChannel) key.channel()).accept(); // получаем канал из ключа
+        String clientName = "client #" + acceptedClientIndex;
+        acceptedClientIndex++;
+        sc.configureBlocking(false);
+        sc.register(selector, SelectionKey.OP_READ, clientName);// регистрируем ключ на чтение и предаем вместе с именем клиента
+        sc.write(welcomeBuf);
+        welcomeBuf.rewind();
+        log.info("Подключился новый клиент " + clientName);
+    }
+
+
+    /**
+     * Чтение из канала
+     *
+     * @param key ключ
+     * @throws IOException исключени
+     */
+    private void handleRead(SelectionKey key) throws IOException {
+        SocketChannel ch = (SocketChannel) key.channel();
+        StringBuilder sb = new StringBuilder();
+
+        buf.clear(); //очищаем буфер
+        int read;
+        while ((read = ch.read(buf)) > 0) { //читаем из канала
+            buf.flip();
+            byte[] bytes = new byte[buf.limit()];
+            buf.get(bytes); //записываем в буфер
+            sb.append(new String(bytes));
+            buf.clear();
+        }
+        String msg;
+        if (read < 0) {
+            msg = key.attachment() + " покинул чат\n";
+            ch.close();
+        } else {
+            msg = key.attachment() + ": " + sb.toString();
+        }
+
+        System.out.println(msg);
+        broadcastMessage(msg);
+    }
+
+    private void broadcastMessage(String msg) throws IOException {
+        ByteBuffer msgBuf = ByteBuffer.wrap(msg.getBytes());
+        for (SelectionKey key : selector.keys()) {
+            if (key.isValid() && key.channel() instanceof SocketChannel) {
+                SocketChannel sch = (SocketChannel) key.channel();
+                sch.write(msgBuf);
+                msgBuf.rewind();
+            }
+        }
     }
 }
