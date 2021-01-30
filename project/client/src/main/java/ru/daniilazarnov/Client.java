e3c031832f90d93a41ad22667a2ffcdd133f916b
Valeriy Lazarev <v.lazarev.spb@gmail.com>
2021-01-30 17:34:50 +0300

    server no longer crashes when disconnected client

--- project/client/src/main/java/ru/daniilazarnov/Client.java
+++ project/client/src/main/java/ru/daniilazarnov/Client.java
@@ -11,6 +11,8 @@ import java.nio.channels.AlreadyConnectedException;
 import java.nio.channels.AsynchronousCloseException;
 import java.nio.channels.Selector;
 import java.nio.channels.SocketChannel;
+import java.sql.Timestamp;
+import java.util.Date;
 
 public class Client {
 
@@ -33,7 +35,6 @@ public class Client {
     }
 
     public static void main(String[] args) throws IOException {
-
         client = SocketChannel.open();
         String msg = "Hello\n";
         String hostname = "localhost";
@@ -43,10 +44,15 @@ public class Client {
         buffer = sendMessage(client, msg);
         readingInMessage(client, buffer);
 
-        System.out.println();
-        while (client.isOpen()) {
-
-        }
+//        while (client.isOpen()) {
+//            long before = System.currentTimeMillis();
+//            long after = 0L;
+//            if (before - after >= 1000) {
+//                log.info("client.isOpen()");
+//            }
+//            after = System.currentTimeMillis();
+//
+//        }
     }
 
     private static void connect(String hostname, int port) throws IOException {
@@ -78,21 +84,36 @@ public class Client {
     //    @org.jetbrains.annotations.NotNull
     private static ByteBuffer sendMessage(SocketChannel client, String msg) throws IOException {
         ByteBuffer buffer = ByteBuffer.wrap(msg.getBytes());
-        log.debug(buffer);
+//        log.debug(buffer);
         client.write(buffer);
         return buffer;
     }
 
     private static void readingInMessage(SocketChannel client, ByteBuffer buffer) throws IOException {
-        int i = client.read(buffer);
-        while (i != -1) {
-            buffer.flip(); //Переключение буфера в режим чтения
+        SocketChannel ch = client.socket().getChannel();
+        StringBuilder sb = new StringBuilder();
+//        int read;
+        buffer.clear(); //очищаем буфер
+        int read;
+//        while (read != -1) {
 
-            while (buffer.hasRemaining()) {
-                System.out.print((char) buffer.get()); // побайтово получаем содержимое буфера
-            }
+        while ((read = ch.read(buffer)) != -1) { //читаем из канала
+            int positions = buffer.position();
+            buffer.flip();
+            byte[] bytes = new byte[buffer.limit()];
+            buffer.get(bytes); //записываем в данные из буфера в массив
+            sb.append(new String(bytes)); // добавляем в стринг билдер
             buffer.clear();
-            i = client.read(buffer); //получаем следующий фрагмент
+            if (positions < 6) {
+                break;
+            }
         }
+        log.debug("sb: " + sb.toString());
+        if (read < 0) {
+            ch.close();
+        }
+
+
+        log.debug("Out of the loop");
     }
 }
--- project/server/src/main/java/ru/daniilazarnov/Server.java
+++ project/server/src/main/java/ru/daniilazarnov/Server.java
@@ -22,11 +22,15 @@ public class Server implements Runnable {
         this.serverSocketChannel.socket().bind(new InetSocketAddress(8189)); //устанавливаем порт для ССЧ, который он будет слушать
         this.serverSocketChannel.configureBlocking(false);
         this.selector = Selector.open();
-        this.serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT); // регистрация селектора на канналше с определенным ключом
+        this.serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT); // регистрация селектора на каннале с определенным ключом акцепт
     }
 
     public static void main(String[] args) throws IOException {
-        new Thread(new Server()).start();
+        start();
+    }
+
+    private static void start() throws IOException {
+        new Thread(new Server()).start(); // запускаем сервер
     }
 
     @Override
@@ -36,61 +40,70 @@ public class Server implements Runnable {
             Iterator<SelectionKey> iter;
             SelectionKey key;
             while (this.serverSocketChannel.isOpen()) { // внутри вайла канал открыт
-                selector.select();// ждем новых ключей
+                selector.select();// ждем новых событий
                 iter = this.selector.selectedKeys().iterator();
-                while (iter.hasNext()) { // перебираем ключи в итераторе
-                    key = iter.next();
-                    iter.remove();
+                while (iter.hasNext()) { // перебираем ключи в итераторе событий
+                    key = iter.next(); //получаем ссылку на событие
+                    iter.remove(); //удаляем событие из списка обработки
                     if (key.isAcceptable()) { // проверяем ключ
-                        handleAccept(key);
+                        handleAccept(key); // если подключился
                     }
-                    if (key.isReadable()) {
+                    if (key.isReadable()) { //если что-то написал
                         handleRead(key);
                     }
                 }
             }
         } catch (IOException e) {
-//            e.printStackTrace();
+
             log.error(e.getLocalizedMessage());
+            e.printStackTrace();
         }
     }
 
     /**
      * Метод подключает новых клиентов
+     *
      * @param key Ключ выбора создается каждый раз, когда канал регистрируется с помощью селектора.
      *            Ключ остается действительным до тех пор, пока он не будет отменен вызовом его метода cancel
      * @throws IOException исключение
      */
     private void handleAccept(SelectionKey key) throws IOException {
-        SocketChannel sc = ((ServerSocketChannel) key.channel()).accept(); // получаем канал из ключа
-        String clientName = "client #" + acceptedClientIndex;
+        SocketChannel sc = ((ServerSocketChannel) key.channel()).accept(); // получаем канал из ключа, ссылка на ССК
+        String clientName = "client #" + acceptedClientIndex; // присваеваем индекс клиенту
         acceptedClientIndex++;
-        sc.configureBlocking(false);
-        sc.register(selector, SelectionKey.OP_READ, clientName);// регистрируем ключ на чтение и предаем вместе с именем клиента
-        sc.write(welcomeBuf);
-        welcomeBuf.rewind();
+        sc.configureBlocking(false); // переключение на неблокирующий режим
+        sc.register(selector, SelectionKey.OP_READ, clientName);// регистрируем ключ в селекторе на чтение и предаем вместе с именем клиента
+        sc.write(welcomeBuf); // посылаем сообщение из велком буфера
+        welcomeBuf.rewind(); //сбрасывает позицию буфера на начало, для повторной отправки
         log.info("Подключился новый клиент " + clientName);
     }
 
 
     /**
+     * Обработка события чтение из канала
      * Чтение из канала
      *
      * @param key ключ
      * @throws IOException исключени
      */
     private void handleRead(SelectionKey key) throws IOException {
-        SocketChannel ch = (SocketChannel) key.channel();
+        SocketChannel ch = (SocketChannel) key.channel(); //получаем ссылку на канал из ключа
         StringBuilder sb = new StringBuilder();
 
         buf.clear(); //очищаем буфер
         int read;
-        while ((read = ch.read(buf)) > 0) { //читаем из канала
-            buf.flip();
-            byte[] bytes = new byte[buf.limit()];
-            buf.get(bytes); //записываем в буфер
-            sb.append(new String(bytes));
-            buf.clear();
+        try {
+            while ((read = ch.read(buf)) > 0) { //читаем из канала
+                buf.flip();
+                byte[] bytes = new byte[buf.limit()];
+                buf.get(bytes); //записываем в данные из буфера в массив
+                sb.append(new String(bytes)); // добавляем в стринг билдер
+                buf.clear();
+
+            }
+        } catch (Exception e) {
+            key.cancel();
+            read = -1;
         }
         String msg;
         if (read < 0) {
@@ -104,13 +117,20 @@ public class Server implements Runnable {
         broadcastMessage(msg);
     }
 
+
+    /**
+     * Рассылаем сообщения всем клиентам
+     *
+     * @param msg сообщение
+     * @throws IOException
+     */
     private void broadcastMessage(String msg) throws IOException {
-        ByteBuffer msgBuf = ByteBuffer.wrap(msg.getBytes());
-        for (SelectionKey key : selector.keys()) {
-            if (key.isValid() && key.channel() instanceof SocketChannel) {
-                SocketChannel sch = (SocketChannel) key.channel();
-                sch.write(msgBuf);
-                msgBuf.rewind();
+        ByteBuffer msgBuf = ByteBuffer.wrap(msg.getBytes()); // оборачиваем собщение в байтбуффер
+        for (SelectionKey key : selector.keys()) { // перебираем список всех ключей подписавшихся
+            if (key.isValid() && key.channel() instanceof SocketChannel) { // если ключи валидные и канал является соектканалом
+                SocketChannel sch = (SocketChannel) key.channel(); // получаем ссылку
+                sch.write(msgBuf); //отправляем сообщение
+                msgBuf.rewind(); // возвращаем буфер в исходное положение
             }
         }
     }
