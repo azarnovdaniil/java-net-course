02cbecc26252b9c555aac0f5152247a9a3a49a19
Nektarine <lupanova-2011@mail.ru>
2021-01-31 18:12:15 +0300

    Homework lesson 1.

--- lesson-1/src/main/java/ru/daniilazarnov/Main.java
+++ lesson-1/src/main/java/ru/daniilazarnov/Main.java
@@ -2,7 +2,8 @@ package ru.daniilazarnov;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Lesson 1");
     }
 }
