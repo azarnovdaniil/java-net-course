1491ce6612896f45d50621208c88e11cfdf03e5b
VAR-VAR <hrom9090@mail.ru>
2021-02-01 00:01:58 +0300

    Решение передачи списка файлов.

--- project/client/src/main/java/ru/daniilazarnov/Client.java
+++ project/client/src/main/java/ru/daniilazarnov/Client.java
@@ -1,8 +1,18 @@
 package ru.daniilazarnov;
 
-public class Client {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-    public static void main(String[] args) {
-        System.out.println("Client!");
+public class Client extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent parent = FXMLLoader.load(getClass().getResource("/cloud.fxml"));
+        primaryStage.setTitle("Chat");
+        primaryStage.setScene(new Scene(parent));
+        primaryStage.show();
     }
 }
--- project/server/src/main/java/ru/daniilazarnov/Server.java
+++ project/server/src/main/java/ru/daniilazarnov/Server.java
@@ -1,8 +1,56 @@
 package ru.daniilazarnov;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.serialization.ClassResolvers;
+import io.netty.handler.codec.serialization.ObjectDecoder;
+import io.netty.handler.codec.serialization.ObjectEncoder;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Server {
 
-    public static void main(String[] args) {
-        System.out.println("Server!");
+    private static final Logger LOG = LoggerFactory.getLogger(Server.class);
+    public Server() throws InterruptedException {
+
+        EventLoopGroup auth = new NioEventLoopGroup();
+        EventLoopGroup worker = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(auth, worker)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(
+//                                    new StringDecoder(),
+//                                    new StringEncoder(),
+//                                    new ObjectDecoder(ClassResolvers.cacheDisabled(null)),
+//                                    new ObjectEncoder(),
+                                    new OutStringHandler(),
+                                    new InStringHandler(),
+                                    new NettyHandler()
+                            );
+                        }
+                    });
+            ChannelFuture channelFuture = bootstrap.bind(8189).sync();
+            LOG.info("Server started");
+            channelFuture.channel().closeFuture().sync(); // block
+        } finally {
+            auth.shutdownGracefully();
+            worker.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        new Server();
     }
 }
