5f5c8b560c3800649537ed9ff9463bcd52a8e1c2
daniilazarnov <azarnow.d@gmail.com>
2021-02-02 09:15:34 +0300

    Update lesson 3

--- lesson-3/src/main/java/ru/daniilazarnov/serialization/Client.java
+++ lesson-3/src/main/java/ru/daniilazarnov/serialization/Client.java
@@ -1,8 +1,27 @@
 package ru.daniilazarnov.serialization;
 
+import io.netty.handler.codec.serialization.ObjectDecoderInputStream;
+import io.netty.handler.codec.serialization.ObjectEncoderOutputStream;
+import ru.daniilazarnov.serialization.domain.MyMessage;
+
+import java.net.Socket;
+
 public class Client {
 
     public static void main(String[] args) {
-        System.out.println("Hello!");
+
+        try (Socket socket = new Socket("localhost", 8189);
+             ObjectEncoderOutputStream oeos = new ObjectEncoderOutputStream(socket.getOutputStream());
+             ObjectDecoderInputStream odis = new ObjectDecoderInputStream(socket.getInputStream(), 100 * 1024 * 1024)) {
+            MyMessage textMessage = new MyMessage("Hello Server!!!");
+
+            ///FileMessage fileMessage = new FileMessage("file.txt", Files.readAllBytes(Path.of("/Users/daniilazarnov/repos/java-course-3/demo.txt")));
+            oeos.writeObject(textMessage);
+            oeos.flush();
+            MyMessage msgFromServer = (MyMessage) odis.readObject();
+            System.out.println("Answer from server: " + msgFromServer.getText());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
