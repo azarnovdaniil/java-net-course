312f1fe17d31cdb057d40aa7eec7f7b99df08e5c
Nektarine <lupanova-2011@mail.ru>
2021-02-04 17:21:19 +0300

    Homework lesson 2

---
+++ lesson-2/src/main/java/ru/daniilazarnov/AuthService.java
@@ -0,0 +1,12 @@
+package ru.daniilazarnov;
+
+
+
+public interface AuthService
+{
+        String getNicknameByLoginAndPassword(String login, String password);
+        boolean registration(String login, String password, String nickname);
+        boolean changeNick(String oldNickname, String newNickname);
+}
+
+
---
+++ lesson-2/src/main/java/ru/daniilazarnov/Server.java
@@ -0,0 +1,105 @@
+package ru.daniilazarnov;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class Server {
+    private ServerSocket server;
+    private Socket socket;
+    private final int PORT = 8189;
+    private List<ClientHandler> clients;
+    private AuthService authService;
+
+    public Server() {
+        clients = new CopyOnWriteArrayList<>();
+
+        if (!SQLHandler.connect()) {
+            throw new RuntimeException("Не удалось подключиться к БД");
+        }
+        authService = new DBAuthServise();
+
+        try {
+            server = new ServerSocket(PORT);
+            System.out.println("server started!");
+
+            while (true) {
+                socket = server.accept();
+                System.out.println("client connected " + socket.getRemoteSocketAddress());
+                new ClientHandler(this, socket);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            SQLHandler.disconnect();
+            System.out.println("server closed");
+            try {
+                server.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void broadcastMsg(ClientHandler sender, String msg) {
+        String message = String.format("%s : %s", sender.getNickname(), msg);
+
+        for (ClientHandler c : clients) {
+            c.sendMsg(message);
+        }
+    }
+
+    public void privateMsg(ClientHandler sender, String receiver, String msg) {
+        String message = String.format("[ %s ] private [ %s ] : %s", sender.getNickname(), receiver, msg);
+
+        for (ClientHandler c : clients) {
+            if (c.getNickname().equals(receiver)) {
+                c.sendMsg(message);
+                if (!c.equals(sender)) {
+                    sender.sendMsg(message);
+                }
+                return;
+            }
+        }
+        sender.sendMsg("Not found user: " + receiver);
+    }
+
+    public void subscribe(ClientHandler clientHandler) {
+        clients.add(clientHandler);
+        broadcastClientList();
+    }
+
+    public void unsubscribe(ClientHandler clientHandler) {
+        clients.remove(clientHandler);
+        broadcastClientList();
+    }
+
+    public AuthService getAuthService() {
+        return authService;
+    }
+
+    public boolean isloginAuthenticated(String login) {
+        for (ClientHandler c : clients) {
+            if (c.getLogin().equals(login)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void broadcastClientList() {
+        StringBuilder sb = new StringBuilder("/clientlist ");
+        for (ClientHandler c : clients) {
+            sb.append(c.getNickname()).append(" ");
+        }
+
+        String msg = sb.toString();
+        for (ClientHandler c : clients) {
+            c.sendMsg(msg);
+        }
+    }
+}
+
