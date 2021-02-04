00c1b3ffd29aecb0bb6c7446124918f998b0eb5e
GradusovRoman <monomari86@gmail.com>
2021-02-04 19:03:28 +0500

    HomeWork_2

--- project/client/src/main/java/ru/daniilazarnov/Client.java
+++ project/client/src/main/java/ru/daniilazarnov/Client.java
@@ -1,8 +1,55 @@
 package ru.daniilazarnov;
 
-public class Client {
+import java.io.*;
+import java.io.ByteArrayOutputStream;
+import java.net.*;
 
-    public static void main(String[] args) {
-        System.out.println("Client!");
+public class Client{
+
+    private final static String fileOutput = "C:\\java_log\\2\\test_out.txt";
+
+    public static void main(String args[]) {
+        byte[] aByte = new byte[1];
+        int bytesRead;
+
+        Socket clientSocket = null;
+        InputStream is = null;
+
+        try {
+            clientSocket = new Socket( "localhost" , 8189 );
+            is = clientSocket.getInputStream();
+        } catch (IOException ex) {
+            System.out.println("Ошибка соединения с сервером");
+
+        }
+
+        ByteArrayOutputStream barros = new ByteArrayOutputStream();
+
+        if (is != null) {
+
+            FileOutputStream fos = null;
+            BufferedOutputStream bos = null;
+            try {
+                fos = new FileOutputStream( fileOutput );
+                bos = new BufferedOutputStream(fos);
+
+                do {
+                    barros.write(aByte);
+                    bytesRead = is.read(aByte);
+                } while (bytesRead != -1);
+
+                bos.write(barros.toByteArray());
+                bos.flush();
+                bos.close();
+                clientSocket.close();
+            } catch (IOException ex) {
+                System.out.println("Ошибка передачи файла, проверьте доступность сревера!");
+
+            }
+
+            System.out.println("Файл отправлен");
+        }
+
     }
+
 }
--- project/server/src/main/java/ru/daniilazarnov/Server.java
+++ project/server/src/main/java/ru/daniilazarnov/Server.java
@@ -1,8 +1,53 @@
 package ru.daniilazarnov;
 
+
+import java.io.*;
+import java.net.*;
+
 public class Server {
 
-    public static void main(String[] args) {
-        System.out.println("Server!");
+    private final static String fileToSend = "C:\\java_log\\1\\test_in.txt";
+
+    public static void main(String args[]) {
+        System.out.println("Сервер запущен, ожидаем передачи файла...");
+        while (true) {
+            ServerSocket ServSocket;
+            Socket clientSocket = null;
+            BufferedOutputStream outToClient = null;
+
+            try {
+                ServSocket = new ServerSocket(8189);
+                clientSocket = ServSocket.accept();
+                outToClient = new BufferedOutputStream(clientSocket.getOutputStream());
+            } catch (IOException ex) {
+
+                          }
+
+            if (outToClient != null) {
+                File myFile = new File( fileToSend );
+                FileInputStream fis = null;
+                byte[] byteArray = new byte[(int) myFile.length()];
+
+                try {
+                    fis = new FileInputStream(myFile);
+                } catch (FileNotFoundException ex) {
+
+                }
+                BufferedInputStream bis = new BufferedInputStream(fis);
+
+                try {
+                    bis.read(byteArray, 0, byteArray.length);
+                    outToClient.write(byteArray, 0, byteArray.length);
+                    outToClient.flush();
+                    outToClient.close();
+                    clientSocket.close();
+                    System.out.println("Файл принят, закрываем соединение...");
+
+                    return;
+                } catch (IOException ex) {
+                    System.out.println("Что-то пошло не так...");
+                }
+            }
+        }
     }
 }
