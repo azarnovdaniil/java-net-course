5f84d50c4806c750193558f849553b6432f90172
Pomikatze <71844275+Pomikatze@users.noreply.github.com>
2021-02-04 17:11:13 +0300

    Add files via upload

---
+++ project/client/src/main/java/ru/daniilazarnov/ClientHandler.java
@@ -0,0 +1,22 @@
+package ru.daniilazarnov;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class ClientHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf m = (ByteBuf) msg;
+        try {
+        } finally {
+            m.release();
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
