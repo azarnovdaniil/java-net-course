8ef782522d8a73e22bfd6353af2565b87ba98838
Pomikatze <71844275+Pomikatze@users.noreply.github.com>
2021-02-04 17:08:03 +0300

    Update Server.java

--- project/server/src/main/java/ru/daniilazarnov/Server.java
+++ project/server/src/main/java/ru/daniilazarnov/Server.java
@@ -1,8 +1,53 @@
 package ru.daniilazarnov;
 
-public class Server {
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
 
-    public static void main(String[] args) {
-        System.out.println("Server!");
+public class Server {
+    private int port;
+
+    public Server (int port){
+        this.port = port;
+    }
+
+    public void run() throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workGroup = new NioEventLoopGroup();
+
+        try{
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new ServerHandler());
+                        }
+                    })
+            .option(ChannelOption.SO_BACKLOG, 128)
+            .childOption(ChannelOption.SO_KEEPALIVE, true);
+
+            ChannelFuture f = b.bind(port).sync();
+            f.channel().closeFuture().sync();
+        } finally {
+            workGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
+        int port;
+        if (args.length > 0){
+            port = Integer.parseInt(args[0]);
+        } else {
+            port = 8080;
+        }
+        new Server(port).run();
     }
 }
