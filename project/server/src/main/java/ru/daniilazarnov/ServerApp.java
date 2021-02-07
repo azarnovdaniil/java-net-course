b03525e6e0ebb22a9bbdaffcec3d3fda1139a52a
Andrey Kataev <corvolk@yandex.ru>
2021-02-07 16:00:43 +0300

    Основа сервера написанная на Netty.

---
+++ project/server/src/main/java/ru/daniilazarnov/ServerApp.java
@@ -0,0 +1,34 @@
+package ru.daniilazarnov;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.ServerChannel;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class ServerApp {
+    public static void main(String[] args) {
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap boot = new ServerBootstrap();
+            boot.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<ServerChannel>() {
+                        @Override
+                        protected void initChannel(ServerChannel serverChannel) throws Exception {
+
+                        }
+                    });
+            ChannelFuture channelFuture = boot.bind(8189).sync();
+            channelFuture.channel().closeFuture().sync();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
