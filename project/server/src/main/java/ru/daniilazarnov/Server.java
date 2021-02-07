c622504754dbd8246415f5bc64c1aa65455f1e7c
VeronikaViskey <veronika.viskey@yandex.ru>
2021-02-08 01:25:17 +0300

    homework for lesson 3

--- project/server/src/main/java/ru/daniilazarnov/Server.java
+++ project/server/src/main/java/ru/daniilazarnov/Server.java
@@ -1,94 +1,35 @@
 package ru.daniilazarnov;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.net.SocketTimeoutException;
-import java.util.Scanner;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
 
-public class Server implements Runnable {
-    private ServerSocket server;
-    private Socket socket;
-    private DataInputStream in;
-    private DataOutputStream out;
-    private final int PORT = 8189;
-    private int acceptedClientIndex = 1;
-    private Scanner scanner = new Scanner(System.in);
+public class Server {
 
-    public static void main(String[] args) throws IOException {
-        new Thread(new Server()).start();
-    }
-
-    @Override
-    public void run() {
-        try{
-            server = new ServerSocket(PORT);
-            System.out.println("Сервер запущен! Порт " + PORT);
-
-            while (true) {
-                socket = server.accept();
-                System.out.println("Клиент # " + acceptedClientIndex + " подключился!");
-                clientHandler();
-                acceptedClientIndex++;
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void broadcastMsg(String msg) {
-        String message = String.format("%s: %s", "Клиент # " + (acceptedClientIndex-1), msg);
-        sendMsg(message);
-        System.out.println(message);
-    }
-
-    public void clientHandler() {
+    public static void main(String[] args) {
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
         try {
-            in = new DataInputStream(socket.getInputStream());
-            out = new DataOutputStream(socket.getOutputStream());
-
-            new Thread(() -> {
-                try {
-                    while (true) {
-                        String str = in.readUTF();
-
-                        if (str.startsWith("/")) {
-
-                            if (str.equals("/end")) {
-                                out.writeUTF("/end");
-                                break;
-                            }
-
-                        } else {
-                            broadcastMsg(str);
+            ServerBootstrap sb = new ServerBootstrap();
+            sb.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(new ServerHandler());
                         }
-                    }
-                }catch (SocketTimeoutException e){
-                    sendMsg("/end");
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } finally {
-                    System.out.println("Клиент отключился!");
-                    try {
-                        socket.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }).start();
-        } catch (IOException e) {
+                    });
+            ChannelFuture future = sb.bind(8189).sync();
+            future.channel().closeFuture().sync();
+        } catch (Exception e) {
             e.printStackTrace();
-        }
-    }
-
-    public void sendMsg(String msg) {
-        try {
-            out.writeUTF(msg);
-        } catch (IOException e) {
-            e.printStackTrace();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
         }
     }
 }
---
+++ project/server/src/main/java/ru/daniilazarnov/ServerHandler.java
@@ -0,0 +1,27 @@
+package ru.daniilazarnov;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class ServerHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("Client connected");
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf = (ByteBuf)msg;
+        while (buf.readableBytes()>0){
+            System.out.println((char) buf.readByte());
+        }
+        buf.release();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
