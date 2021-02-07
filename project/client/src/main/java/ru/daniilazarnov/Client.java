98341ddf1440a9b11141fea3db25f721483a14f0
Andrey Kataev <corvolk@yandex.ru>
2021-02-07 16:20:37 +0300

    Основа клиента на Netty.

--- project/client/src/main/java/ru/daniilazarnov/Client.java
+++ project/client/src/main/java/ru/daniilazarnov/Client.java
@@ -1,8 +1,7 @@
 package ru.daniilazarnov;
 
 public class Client {
-
     public static void main(String[] args) {
-        System.out.println("Client!");
+        ClientApp cl = new ClientApp();
     }
 }
---
+++ project/client/src/main/java/ru/daniilazarnov/ClientApp.java
@@ -0,0 +1,44 @@
+package ru.daniilazarnov;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+
+public class ClientApp {
+    private SocketChannel socket;
+
+    public ClientApp(){
+        new Thread(() -> {
+            EventLoopGroup workerGroup = new NioEventLoopGroup();
+            try {
+                Bootstrap bootstrap = new Bootstrap();
+                bootstrap.group(workerGroup)
+                        .channel(NioSocketChannel.class)
+                        .handler(new ChannelInitializer<SocketChannel>() {
+                            @Override
+                            protected void initChannel(SocketChannel socketChannel) throws Exception {
+                                socket = socketChannel;
+                                socketChannel.pipeline().addLast(new StringDecoder(), new StringEncoder());
+                            }
+                        });
+                ChannelFuture channelFuture = bootstrap.connect("localhost", 8189).sync();
+                channelFuture.channel().closeFuture().sync();
+            } catch (Exception e){
+                e.printStackTrace();
+            }finally {
+                workerGroup.shutdownGracefully();
+            }
+        }).start();
+    }
+
+    public void sendMessage(String message){
+        socket.writeAndFlush(message);
+    }
+
+}
