5e0be7241226bb0c9a5774aecd8f9946aa7d103f
Angelina Kosenko <67165094+Kosenko-A@users.noreply.github.com>
2021-02-07 20:17:12 +0500

    Add files via upload

--- Server.java
+++ Server.java
@@ -1,10 +1,14 @@
 package ru.daniilazarnov;
 
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class Server {
@@ -13,9 +17,11 @@ public class Server {
 
     public static void main(String[] args) throws IOException {
         new Server();
+
     }
 
     public Server() throws IOException {
+
         try {
             ServerSocket serverSocket = new ServerSocket(888);
 
@@ -23,6 +29,9 @@ public class Server {
 
             DataInputStream in = new DataInputStream(clientSocket.getInputStream());
             DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
+
+            EventLoopGroup auth = new NioEventLoopGroup(1);
+            EventLoopGroup worker = new NioEventLoopGroup();
 
             Thread th1 = new Thread() {
                 @Override
@@ -32,13 +41,55 @@ public class Server {
                         String message = null;
                         try {
                             message = in.readUTF();
-                            if (isLs(message)) {
+                            if (message.contains("ls")) {
                                 text = "Введите имя папки, файлы которой хотите вывести";
                                 out.writeUTF(text);
                                 String nameDirectory = in.readUTF();
+                                File dir = new File(nameDirectory);
+                                File[] arrFiles = dir.listFiles();
+                                List<File> lst = Arrays.asList(arrFiles);
+                                out.writeUTF(String.valueOf(lst));
                             }
 
-                            if (isSend(message)) {
+                            if (message.contains("sendFile")){
+                                text = "Введите имя файла, который хотите отправить";
+                                out.writeUTF(text);
+                                String nameFileToSend = in.readUTF();
+
+
+
+
+                            }
+
+                            if (message.contains("delete")) {
+                                text = "Введите имя папки с файлом, который хотите удалить";
+                                out.writeUTF(text);
+                                String nameDirectoryWithFileDelete = in.readUTF();
+                                text = "Введите имя файла, который хотите удалить";
+                                out.writeUTF(text);
+                                String nameFileDelete = in.readUTF();
+                                Path rootPath = Paths.get(nameDirectoryWithFileDelete + "/" + nameFileDelete);
+                                Files.walkFileTree(rootPath, new SimpleFileVisitor<Path>(){
+                                    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                                        System.out.println("delete file: " + file.toString());
+                                        Files.delete(file);
+                                        return FileVisitResult.CONTINUE;
+                                    }
+                                });
+                            }
+
+                            if (message.contains("create")){
+                                text = "Введите имя папки, в которой хотите создать файл";
+                                out.writeUTF(text);
+                                String nameDirectoryWithFileCreate = in.readUTF();
+                                text = "Введите имя файла";
+                                out.writeUTF(text);
+                                String nameFileCreate = in.readUTF();
+                                Path path = Paths.get(nameDirectoryWithFileCreate + "/" + nameFileCreate);
+                                Path newFile = Files.createFile(path);
+                            }
+
+                            if (message.contains("send")) {
                                 try {
                                     text = "Введите имя папки, в которой находится файл";
                                     out.writeUTF(text);
@@ -79,26 +130,6 @@ public class Server {
 
 
                     }
-                }
-
-                public boolean isLs(String message) {
-                    boolean l = false;
-                    if (message.contains("ls")) {
-                        l = true;
-                    } else {
-                        l = false;
-                    }
-                    return l;
-                }
-
-                public boolean isSend(String message) {
-                    boolean s = false;
-                    if (message.contains("send")) {
-                        s = true;
-                    } else {
-                        s = false;
-                    }
-                    return s;
                 }
             };
             th1.start();
