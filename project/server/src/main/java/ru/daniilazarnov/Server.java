a90b2041e1ddeacdbffceb10c5c82883ef5bc76b
ArtemL <plazzza@mail.ru>
2021-02-07 21:09:25 +0300

    HW_2-3

--- project/server/src/main/java/ru/daniilazarnov/Server.java
+++ project/server/src/main/java/ru/daniilazarnov/Server.java
@@ -1,8 +1,40 @@
 package ru.daniilazarnov;
 
-public class Server {
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
 
+public class Server {
     public static void main(String[] args) {
-        System.out.println("Server!");
+
+        EventLoopGroup parentGroup = new NioEventLoopGroup(1);  // пул потоков для инициализации подключений
+        EventLoopGroup childGroup = new NioEventLoopGroup(); // пул потоков для обработки данных
+
+        try {
+            ServerBootstrap srvBoot = new ServerBootstrap(); // установка параметров сервера
+            srvBoot.group(parentGroup, childGroup); // установка пулов потоков
+            srvBoot.channel(NioServerSocketChannel.class);  // не понимаю эту запись, канал для подключения клиентов,
+
+            ChannelInitializer channelInit = new ChannelInitializer<SocketChannel>() { // не понимаю эту запись, SocketChannel информация о соединении
+                @Override
+                protected void initChannel(SocketChannel socketChannel) throws Exception {
+
+                }
+            };
+
+            srvBoot.childHandler(channelInit); // не понимаю эту запись, настройка процесса общения,
+
+            ChannelFuture future = srvBoot.bind(50505).sync(); // привязка порта, запуск сервера, future - информация о какой-то выполняемой задаче.
+            future.channel().closeFuture().sync(); // loseFuture() - ожидание остановки сервера, sync() - запуск ожидания,
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            parentGroup.shutdownGracefully(); // закрытие пула потоков
+            childGroup.shutdownGracefully(); // закрытие пула потоков
+        }
     }
 }
---
+++ project/server/src/main/java/ru/daniilazarnov/ServerHandler.java
@@ -0,0 +1,28 @@
+package ru.daniilazarnov;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import org.w3c.dom.ls.LSOutput;
+
+public class ServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {  // срабатывает при подключении клиента
+        System.out.println("клиент подключился");
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf = (ByteBuf) msg;
+        while(buf.readableBytes() > 0){
+
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
