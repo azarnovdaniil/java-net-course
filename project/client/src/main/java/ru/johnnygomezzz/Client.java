b5c78bb40f63ea5f0ebf3a86f266071d7f3bbd97
JohnnyGomezzz <seldesign@mail.ru>
2021-02-08 13:01:19 +0300

    Изменения с работы

--- project/client/src/main/java/ru/johnnygomezzz/Client.java
+++ project/client/src/main/java/ru/johnnygomezzz/Client.java
@@ -1,23 +1,36 @@
 package ru.johnnygomezzz;
 
-import java.io.DataOutputStream;
+import io.netty.channel.*;
+import io.netty.handler.codec.serialization.ObjectDecoderInputStream;
+import io.netty.handler.codec.serialization.ObjectEncoderOutputStream;
+
+import java.io.IOException;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class Client {
-    public static void main(String[] args) {
-        try {
-            Socket socket = new Socket("localhost", 8189);
-            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-            Scanner in = new Scanner(socket.getInputStream());
-            out.write(new byte[]{21, 21, 21});
-            String x = in.nextLine();
-            System.out.println("A: " + x);
-            in.close();
-            out.close();
-            socket.close();
-        } catch (Exception e) {
-            e.printStackTrace();
+public class Client extends ChannelInboundHandlerAdapter {
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        try (Socket socket = new Socket("localhost", 8189);
+             ObjectEncoderOutputStream out = new ObjectEncoderOutputStream(socket.getOutputStream());
+             ObjectDecoderInputStream in = new ObjectDecoderInputStream(socket.getInputStream(), 100 * 1024 * 1024)
+        ) {
+
+            while (true) {
+                Scanner scanner = new Scanner(System.in);
+                String msg = scanner.nextLine();
+
+                String textMessage = msg;
+                out.writeObject(textMessage);
+                out.flush();
+
+                //String str = String.valueOf(in.readObject());
+                System.out.println(msg);
+
+
+            }
         }
     }
+
+
 }
---
+++ project/server/src/main/java/ru/johnnygomezzz/handlers/ClientHandler.java
@@ -0,0 +1,54 @@
+package ru.johnnygomezzz.handlers;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class ClientHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("Клиент подключился ");
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) {
+        System.out.println("Клиент отключился");
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+
+        Path path = Paths.get(
+                "e:/_java/geekbrains/java-net-course/project/server/src/main/resources/test.txt");
+
+        String str = "Тестовый текст!!!11";
+        byte[] bs = str.getBytes();
+        Path writtenFilePath = Files.write(path, bs);
+
+        System.out.println("Записано в файл:\n"+ new String(Files.readAllBytes(writtenFilePath)));
+
+        if (msg.equals("/show"))
+        {
+            System.out.println(path.toString());
+        }
+        else {
+            System.out.println("Сообщение от клиента: " + msg);
+            ctx.writeAndFlush(msg);
+        }
+
+        if (msg.equals("/quit"))
+        {
+            ctx.close();
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
