52478c2bfadbd39de636ac3a882fa7bbd91f8836
Aleksandr <AleksandrSivachenko7@gmail.com>
2021-02-08 17:43:48 +1000

    Homework_3

--- project/client/src/main/java/ru/daniilazarnov/Client.java
+++ project/client/src/main/java/ru/daniilazarnov/Client.java
@@ -1,8 +1,36 @@
 package ru.daniilazarnov;
 
+import java.io.*;
+import java.net.Socket;
+
 public class Client {
 
     public static void main(String[] args) {
         System.out.println("Client!");
+        try (
+                Socket socket = new Socket("127.0.0.1", 8000);
+
+                BufferedWriter writer =
+                        new BufferedWriter(
+                                new OutputStreamWriter(
+                                        socket.getOutputStream()));
+                BufferedReader reader =
+                        new BufferedReader(
+                                new InputStreamReader(
+                                        socket.getInputStream()));
+                )
+        {
+            System.out.println("Connected to server");
+            String request = "Hello";
+            writer.write(request);
+            writer.newLine();
+            writer.flush();
+
+            String response = reader.readLine();
+            System.out.println("Response: " + response);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- project/server/src/main/java/ru/daniilazarnov/Server.java
+++ project/server/src/main/java/ru/daniilazarnov/Server.java
@@ -1,8 +1,41 @@
 package ru.daniilazarnov;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
 
     public static void main(String[] args) {
-        System.out.println("Server!");
+        System.out.println("Server started!");
+        try (ServerSocket server = new ServerSocket(8000))
+        {
+
+            while (true)
+                try (
+                        Socket socket = server.accept();
+                        BufferedWriter writer =
+                              new BufferedWriter(
+                                      new OutputStreamWriter(
+                                              socket.getOutputStream()));
+                        BufferedReader reader =
+                              new BufferedReader(
+                                      new InputStreamReader(
+                                              socket.getInputStream()))
+            ) {
+                    String request = reader.readLine();
+                    System.out.println("Request: +" + request);
+                    String response = "Hello from server";
+                    System.out.println(response);
+                    writer.write(response);
+                    writer.newLine();
+                    writer.flush();
+                } catch (NullPointerException e) {
+                    e.printStackTrace();
+                }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
