b7c784176d0fb6023f9eb2ccf742b78fcfde4b95
GradusovRoman <monomari86@gmail.com>
2021-02-08 20:52:54 +0500

    HomeWork_3

---
+++ lesson-3/src/main/java/ru/daniilazarnov/discard/ClientHandler.java
@@ -0,0 +1,24 @@
+package ru.daniilazarnov.discard;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+
+public class ClientHandler extends SimpleChannelInboundHandler<String>{
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
+        System.out.println(msg);
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println(msg);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
