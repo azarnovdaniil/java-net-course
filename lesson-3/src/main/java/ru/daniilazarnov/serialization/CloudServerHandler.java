c4c94e3dca27a335cdb3fa0baefa500f3cf44062
Pomikatze <roma.boxxx@gmail.com>
2021-02-08 19:06:33 +0300

    Lesson_4

--- lesson-3/src/main/java/ru/daniilazarnov/serialization/CloudServerHandler.java
+++ lesson-3/src/main/java/ru/daniilazarnov/serialization/CloudServerHandler.java
@@ -2,6 +2,7 @@ package ru.daniilazarnov.serialization;
 
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
 import ru.daniilazarnov.serialization.domain.FileMessage;
 import ru.daniilazarnov.serialization.domain.MyMessage;
 
@@ -24,21 +25,28 @@ public class CloudServerHandler extends ChannelInboundHandlerAdapter {
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
-        System.out.println(msg.getClass().getName());
-        if (msg instanceof MyMessage) {
-            System.out.println("Client text message: " + ((MyMessage) msg).getText());
-            ctx.writeAndFlush(new MyMessage("Hello Client!"));
-        }
-        if (msg instanceof FileMessage) {
-            System.out.println("save file..");
-            ctx.writeAndFlush(new MyMessage("Your file was succsefuly save"));
-            try {
-                Files.write(Path.of(((FileMessage) msg).getFileName()), ((FileMessage) msg).getContent(), StandardOpenOption.CREATE_NEW);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } else {
-            System.out.printf("Server received wrong object!");
+//        System.out.println(msg.getClass().getName());
+//        if (msg instanceof MyMessage) {
+//            System.out.println("Client text message: " + ((MyMessage) msg).getText());
+//            ctx.writeAndFlush(new MyMessage("Hello Client!"));
+//        }
+//        if (msg instanceof FileMessage) {
+//            System.out.println("save file..");
+//            ctx.writeAndFlush(new MyMessage("Your file was succsefuly save"));
+//            try {
+//                Files.write(Path.of(((FileMessage) msg).getFileName()), ((FileMessage) msg).getContent(), StandardOpenOption.CREATE_NEW);
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        } else {
+//            System.out.printf("Server received wrong object!");
+//        }
+
+        try{
+            System.out.println(msg.toString());
+            ctx.writeAndFlush(msg);
+        } finally {
+            ReferenceCountUtil.release(msg);
         }
     }
 
