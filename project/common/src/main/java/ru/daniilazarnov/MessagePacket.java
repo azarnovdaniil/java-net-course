1cc407bea7ee57983843fc6460da7521240a8887
Lina Rusakova <xlinaris@gmail.com>
2021-02-10 01:39:50 +0300

    Изменила и лополнила состав пакетов сообщений (common class "MessagePacket") - добавила количество сегментов, на которые разбивается сообщений/файл для пересылки, а также  номер сегмента пересылаемого в данный момент to do: реализовать десереализацию  объекта на стороне сервера рассмотреть вариант ключевой пары "имя, пароль" (hashMap) в качестве механизма авторизации на сервер и унификации доступа к файлам в модуле работы с файлами. реализовать common class "FileService" - для сборки/разборки пакетов сообщений реализовать common class "FileManager" - функциональный класс для работы с файловой системой

--- project/common/src/main/java/ru/daniilazarnov/MessagePacket.java
+++ project/common/src/main/java/ru/daniilazarnov/MessagePacket.java
@@ -7,14 +7,19 @@ import java.io.Serializable;
 /**
  * Class MessagePacket определяет универасльный пакет - содержание (состав) сообщений, пересылаемых между клиентом и сервер.
  * Каждое собщение состоит из:
- * - команды к серверу (Enum),
- * - путь для сохранения файла, включая имя файла (String) (для простоты реалзиации)
+ *  - команды к серверу (Enum),
+ *  - путь для сохранения файла, включая имя файла (String) (для простоты реалзиации - "имя клиента / имя файла")
  *  - содержимое для записи в файл (String) (для простоты реализации)
+ *  - число дробных частей (int), на которые разбито "сообщение"
+ *  - номер дробной части (int), передаваемое в данный момент
  */
 public class MessagePacket implements Serializable {
     private commandToServer commandToServer;
     private String pathToFileName;
-    private String contentMessage;
+    private byte content;
+    private int segment;
+    private int allSegments;
+
 
     public ru.daniilazarnov.commandToServer getCommandToServer() {
         return commandToServer;
@@ -32,13 +37,19 @@ public class MessagePacket implements Serializable {
         this.pathToFileName = pathToFileName;
     }
 
-    public String getContentMessage() {
-        return contentMessage;
+    public byte getContent() {
+        return content;
     }
 
-    public void setContentMessage(String contentMessage) {
-        this.contentMessage = contentMessage;
+    public void setContent(byte content) {
+        this.content = content;
     }
 
-
+    public MessagePacket(ru.daniilazarnov.commandToServer commandToServer, String pathToFileName, byte content, int segment, int allSegments) {
+        this.commandToServer = commandToServer;
+        this.pathToFileName = pathToFileName;
+        this.content = content;
+        this.segment = segment;
+        this.allSegments = allSegments;
+    }
 }
