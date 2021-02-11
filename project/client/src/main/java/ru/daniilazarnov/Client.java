dd6e9d89ca19b06ca6dfa40e9af044ccb55a4ee0
CAlina95 <71849889+CAlina95@users.noreply.github.com>
2021-02-11 15:31:03 +0000

    Update Client.java
    Client side!

--- project/client/src/main/java/ru/daniilazarnov/Client.java
+++ project/client/src/main/java/ru/daniilazarnov/Client.java
@@ -2,7 +2,33 @@ package ru.daniilazarnov;
 
 public class Client {
 
+     private static boolean cycleOn = true;
+    private static final int U = 1;
+    private static final int D = 2;
+    private static final int DE = 3;
+    private static final int V = 4;
+    private static final int Q = 5;
+
     public static void main(String[] args) {
-        System.out.println("Client!");
+        Commands.printMessage("Добрый день! \nВведите логин и пароли "); // Проверка log1 pass123 ; log2 pass456 ; log3 pass789
+        do {
+            Controller.doAuthorization();
+        } while (Controller.getName() == null);
+        do {
+            Commands.printMessage("Введите команду:\n1.Загрузить на сервер\n2.Скачать с сервера\n3.Удалить\n4.Список файлов\n0.Выход");
+            int answer = Commands.getMenuItems();
+            switch (answer) {
+                case (U) -> Controller.upload();
+                case (D) -> Controller.download();
+                case (DE) -> Controller.delete();
+                case (V) -> Controller.view();
+                case (Q) -> quit();
+            }
+        } while (cycleOn);
+    }
+
+    private static void quit() {
+        Commands.printMessage("До встречи!");
+        cycleOn = false;
     }
 }
