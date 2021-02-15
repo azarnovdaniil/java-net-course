67adf6a39323aef5b3c631f49c32e922c8148341
Davydov <davydovvip@gmail.com>
2021-02-15 21:45:48 +0500

    added discard server and properties

--- project/server/src/main/java/ru/daniilazarnov/ServerApp.java
+++ project/server/src/main/java/ru/daniilazarnov/ServerApp.java
@@ -3,6 +3,6 @@ package ru.daniilazarnov;
 public class ServerApp {
   public static void main(String[] args) throws InterruptedException {
     Server app = new Server();
-    app.start(8989);
+    app.start();
   }
 }
