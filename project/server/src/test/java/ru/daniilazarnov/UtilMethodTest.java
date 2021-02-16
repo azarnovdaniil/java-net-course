301777a918b3ba8a2755c6403a4f143f3222bc32
Valeriy Lazarev <v.lazarev.spb@gmail.com>
2021-02-16 23:06:49 +0300

    added Constants.java, ProgressBar.java

--- project/server/src/test/java/ru/daniilazarnov/UtilMethodTest.java
+++ project/server/src/test/java/ru/daniilazarnov/UtilMethodTest.java
@@ -11,31 +11,20 @@ import java.nio.file.Paths;
 import static org.junit.jupiter.api.Assertions.*;
 
 class UtilMethodTest {
-    Path pathServerStorages = Paths.get("cloud_storage","user1","_file.txt" );
-    @Test
-    void createFileTest() throws IOException {
-
-//        Files.delete(path.getParent());
-    }
-
     @Test
     void createFolderTest() throws IOException {
         Path path = Paths.get("cloud_storage", "testUser");
         UtilMethod.createFolder("testUser");
         assertTrue(Files.exists(path));
         Files.delete(path);
-
     }
 
     @Test
     void testFile(){
         String HOME_FOLDER_PATH = Paths.get ("local_storage").toString();
         String fileName = "fileclient";
-        Path path = Paths.get("project","client", "local_storage");
+        Path path = Paths.get("project");
         System.out.println(path);
         System.out.println(Files.exists(path));
-
     }
-
-
 }
