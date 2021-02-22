3d749ff02ddb419f554d1fa6cc79b09a33ed3048
VeronikaViskey <veronika.viskey@yandex.ru>
2021-02-22 19:25:42 +0300

    homework for lesson 7

--- project/server/src/main/java/ru/daniilazarnov/Server.java
+++ project/server/src/main/java/ru/daniilazarnov/Server.java
@@ -7,44 +7,41 @@ import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
-import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
-import ru.daniilazarnov.handler.InboundHandler;
-
-import java.nio.ByteOrder;
-import java.util.logging.Logger;
+import io.netty.handler.codec.serialization.ClassResolvers;
+import io.netty.handler.codec.serialization.ObjectDecoder;
+import io.netty.handler.codec.serialization.ObjectEncoder;
+import ru.daniilazarnov.handlers.ServerHandler;
 
 public class Server {
+    private static final int SIZE = 100 * 1024 * 1024;
+    private static final int PORT = 8189;
 
-    private static final int PART_SIZE = 10 * 1024 * 1024;
-    private static java.util.logging.Logger logger = Logger.getLogger("");
-    private int port;
-    private String storageDir = "storage";
-
-    public Server(int port) {
-        this.port = port;
+    public static void main(String[] args) throws Exception {
+        new Server().server();
     }
 
-    public void run() throws Exception {
+    public void server() throws InterruptedException {
+
         EventLoopGroup bossGroup = new NioEventLoopGroup();
         EventLoopGroup workerGroup = new NioEventLoopGroup();
         try {
             ServerBootstrap b = new ServerBootstrap();
-            b.group(bossGroup, workerGroup)
-                    .channel(NioServerSocketChannel.class)
+            b.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class)
                     .childHandler(new ChannelInitializer<SocketChannel>() {
-                        @Override
-                        public void initChannel(SocketChannel ch) {
-                            ch.pipeline()
-                                    .addLast(new LengthFieldBasedFrameDecoder(ByteOrder.BIG_ENDIAN, PART_SIZE, 1, 4, -5, 0, true))
-                                    .addLast(new InboundHandler());
-                        }
-                    });
-            ChannelFuture f = b.bind(port).sync();
-            f.channel().closeFuture().sync();
+
+                protected void initChannel(SocketChannel socketChannel) throws Exception { socketChannel
+                        .pipeline()
+                        .addLast(new ObjectDecoder(SIZE, ClassResolvers.cacheDisabled(null)))
+                        .addLast(new ObjectEncoder())
+                        .addLast(new ServerHandler());
+                }
+            });
+
+            ChannelFuture future = b.bind(PORT).sync();
+            future.channel().closeFuture().sync();
         } finally {
-            workerGroup.shutdownGracefully();
             bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
         }
     }
-
 }
