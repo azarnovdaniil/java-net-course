356659e8403dc09c041f03fb944532e0f7d659b8
Stas <stas.chernomorets@icloud.com>
2021-03-29 23:39:50 +0800

    HomeWork3

---
+++ project/client/src/main/java/ru/daniilazarnov/Network.java
@@ -0,0 +1,64 @@
+package ru.daniilazarnov;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
+import java.util.concurrent.CountDownLatch;
+
+public class Network {
+    private static final Network NETWORK = new Network();
+    public static final int PORT = 8180;
+
+    public static Network getInstance() {
+        return NETWORK;
+    }
+
+    private Network() {
+    }
+
+    private Channel currentChannel;
+
+    public Channel getCurrentChannel() {
+        return currentChannel;
+    }
+
+    public void start(CountDownLatch countDownLatch) {
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap clientBootstrap = new Bootstrap();
+            clientBootstrap.group(group)
+                    .channel(NioSocketChannel.class)
+                    .remoteAddress(new InetSocketAddress("localhost", PORT))
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast();
+                            currentChannel = socketChannel;
+                        }
+                    });
+            ChannelFuture channelFuture = clientBootstrap.connect().sync();
+            countDownLatch.countDown();
+            channelFuture.channel().closeFuture().sync();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                group.shutdownGracefully().sync();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void stop() {
+        currentChannel.close();
+    }
+}
--- project/server/src/main/java/ru/daniilazarnov/ServerHandler.java
+++ project/server/src/main/java/ru/daniilazarnov/ServerHandler.java
@@ -1,46 +1,103 @@
 package ru.daniilazarnov;
 
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.SimpleChannelInboundHandler;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ServerHandler extends SimpleChannelInboundHandler<String> {
-    private static final List<Channel> channels = new ArrayList<>();
-    private static int newClientIndex = 1;
-    private String clientName;
+public class ServerHandler extends ChannelInboundHandlerAdapter {
+    public static final byte MAGIC_BYTE = (byte) 25;
+    private State currentState = State.IDLE;
+    private int nextLength;
+    private String fileName;
+    private long fileLength;
+    private long receivedFileLength;
+    private BufferedOutputStream out;
+    private FileOutputStream fos;
+    private File f;
 
     @Override
-    public void channelActive(ChannelHandlerContext ctx) throws Exception {
-        System.out.println("Клиент подключился: " + ctx);
-        channels.add(ctx.channel());
-        clientName = "Клиент #" + newClientIndex;
-        newClientIndex++;
-        broadcastMessage("SERVER", "Подключился новый клиент: " + clientName);
-    }
-
-    @Override
-    protected void channelRead0(ChannelHandlerContext channelHandlerContext, String s) throws Exception {
-        System.out.println("Получено сообщение: " + s);
-        if (s.startsWith("/")) {
-            if (s.startsWith("/changename ")) { // /changename myname1
-                String newNickname = s.split("\\s", 2)[1];
-                broadcastMessage("SERVER", "Клиент " + clientName + " сменил ник на " + newNickname);
-                clientName = newNickname;
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf = ((ByteBuf) msg);
+        while (buf.readableBytes() > 0) {
+            //Проверка ключа операции
+            if (currentState == State.IDLE) {
+                byte readed = buf.readByte();
+                if (readed == MAGIC_BYTE) {
+                    currentState = State.NAME_LENGTH;
+                    receivedFileLength = 0L;
+                    System.out.println("STATE: Start file receiving");
+                } else {
+                    System.out.println("ERROR: Invalid first byte - " + readed);
+                }
             }
-            return;
+
+            //Длинна названия файла
+            if (currentState == State.NAME_LENGTH) {
+                if (buf.readableBytes() >= (Integer.SIZE / Byte.SIZE)) {
+                    System.out.println("STATE: Get filename length "+nextLength);
+                    nextLength = buf.readInt();
+                    currentState = State.NAME;
+                }
+            }
+
+             //Имя файла
+            if (currentState == State.NAME) {
+                if (buf.readableBytes() >= nextLength) {
+                    byte[] fName = new byte[nextLength];
+                    buf.readBytes(fName);
+                    System.out.println("STATE: Filename received - _" + new String(fName, "UTF-8"));
+                   // out = new BufferedOutputStream(new FileOutputStream("_" + new String(fName)));
+                    currentState = State.FILE_LENGTH;
+                    fileName = new String(fName,"UTF-8");
+                    fos = new FileOutputStream("C:\\Users\\Stas\\Desktop\\Java.NET\\java-net-course\\project\\server\\src\\main\\java\\ru\\daniilazarnov\\" +fileName);
+                   // fos = new FileOutputStream(f);
+                }
+
+            }
+            //Длинна файла
+            if (currentState == State.FILE_LENGTH) {
+                if (buf.readableBytes() >= (Long.SIZE / Byte.SIZE)) {
+                    fileLength = buf.readLong();
+                    System.out.println("STATE: File length received - " + fileLength);
+                    currentState = State.FILE;
+                }
+            }
+            //Содержание файла
+            if (currentState == State.FILE) {
+int count;
+                while (buf.readableBytes() > 0) {
+                   //out.write(buf.readByte());
+                    byte [] b = new byte[(int)fileLength];
+                    buf.readBytes(b);
+
+                    fos.write(b,0,(int)fileLength);
+                    receivedFileLength++;
+                    if (fileLength == receivedFileLength) {
+                        currentState = State.IDLE;
+                        System.out.println("File received");
+                        fos.close();
+                        out.close();
+                        break;
+                    }
+                }
+            }
         }
-        broadcastMessage(clientName, s);
+        if (buf.readableBytes() == 0) {
+            buf.release();
+        }
     }
 
-    public void broadcastMessage(String clientName, String message) {
-        String out = String.format("[%s]: %s\n", clientName, message);
-        for (Channel c : channels) {
-            c.writeAndFlush(out);
-        }
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
     }
-
-
 }
