54fc102dd5c7c2fab79c15ff005825b3102d05e1
Tatiana Nosova <tatiananosova746@gmail.com>
2021-03-29 22:36:53 -0500

    added client via bootstrap

--- project/client/src/main/java/ru/daniilazarnov/Client.java
+++ project/client/src/main/java/ru/daniilazarnov/Client.java
@@ -1,8 +1,51 @@
 package ru.daniilazarnov;
 
-public class Client {
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringEncoder;
+import org.apache.log4j.Logger;
 
-    public static void main(String[] args) {
-        System.out.println("Client!");
+public class Client {
+    private static final Logger log = Logger.getLogger(Client.class);
+
+    public static final int PORT = 8189;
+    public static final String LOCALHOST = "localhost";
+
+    public static void main(String[] args) throws Exception {
+        log.info("Client starting!");
+
+        String host = LOCALHOST;
+        int port = PORT;
+
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(workerGroup);
+            b.channel(NioSocketChannel.class);
+            b.option(ChannelOption.SO_KEEPALIVE, true);
+            b.handler(new ChannelInitializer<SocketChannel>() {
+
+                @Override
+                public void initChannel(SocketChannel ch) {
+                    ch.pipeline()
+                            .addLast(new StringEncoder(),
+                                    new StringEncoder(),
+                                    new ClientHandler());
+                }
+            });
+
+            ChannelFuture f = b.connect(host, port).sync();
+
+            f.channel().closeFuture().sync();
+        } finally {
+            workerGroup.shutdownGracefully();
+        }
     }
 }
