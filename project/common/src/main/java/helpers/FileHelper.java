3648928157cd6864f5301bf2844578cec5245620
Aleksey <lexa709@mail.ru>
2021-04-01 10:27:39 +0300

    lesson4 текущее

--- project/common/src/main/java/helpers/FileHelper.java
+++ project/common/src/main/java/helpers/FileHelper.java
@@ -1,57 +1,20 @@
 package helpers;
 
 import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class FileHelper {
 
     private static final String OUTPUT_DIRECTORY = "upload";
-
-    public static void writeHistory(String message) {
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter("history.txt", true))) {
-            bw.newLine();
-            bw.write(message);
-            bw.flush();
-        } catch (Exception e) {
-            throw new RuntimeException("history writing error", e);
-        }
-    }
-
-    public static String readHistory(int count) {
-        StringBuilder result = new StringBuilder();
-        File file = new File("history.txt");
-
-        if (file.exists()) {
-            try (RandomAccessFile raf = new RandomAccessFile(file, "r")) {
-                StringBuilder tmp = new StringBuilder();
-                long fileLength = raf.length() - 1;
-                raf.seek(fileLength);
-                int i = 0;
-                long pointer = fileLength;
-
-                do {
-                    for (; pointer >= 0; pointer--) {
-                        raf.seek(pointer);
-                        char c = (char) raf.read();
-                        if (c == '\n') {
-                            i++;
-                            pointer--;
-                            break;
-                        }
-                        tmp.append(c);
-                    }
-                    tmp.append("\n");
-                    result.insert(0, tmp.reverse());
-                    tmp.setLength(0);
-                } while (i <= count && pointer >= 0);
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        return result.toString();
-    }
+    private static final String USERNAME_WARNING = "имя пользователя должно быть задано";
+    private static final String PATH_WARNING = "путь должен быть задан";
 
     public static void saveFile(InputStream from, Path path) {
         Path out = Path.of(OUTPUT_DIRECTORY + File.separator + path.getFileName());
@@ -60,5 +23,69 @@ public class FileHelper {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    //todo доделать
+    public static byte[] getFile(String username, String filename) throws IOException {
+        if(username.isEmpty()) throw new IllegalArgumentException(USERNAME_WARNING);
+        if(filename.isEmpty()) throw new IllegalArgumentException(PATH_WARNING);
+
+        byte[] buffer = new byte[0];
+        ByteBuffer buf = ByteBuffer.allocate(48);
+
+        Path p = Path.of("..", OUTPUT_DIRECTORY,username, filename);
+        try (RandomAccessFile src = new RandomAccessFile(p.toFile(), "rw")) {
+            FileChannel srcChannel = src.getChannel();
+        }
+
+        return buffer;
+    }
+
+    public static List<Path> listDirectories(String username) throws IOException {
+        if(username.isEmpty()) throw new IllegalArgumentException(USERNAME_WARNING);
+
+        ArrayList<Path> result = new ArrayList<>();
+
+        Path p = Path.of("..", OUTPUT_DIRECTORY, username);
+
+        if (Files.exists(p)) {
+            try (Stream<Path> stream = Files.walk(p)) {
+                result = (ArrayList<Path>) stream.filter(x -> x.toFile().isDirectory()).collect(Collectors.toList());
+            }
+        }
+
+        return result;
+    }
+
+    public static List<Path> listFilesInDirectory(String username, String name) throws IOException {
+        if(username.isEmpty()) throw new IllegalArgumentException(USERNAME_WARNING);
+        if(name.isEmpty()) throw new IllegalArgumentException(PATH_WARNING);
+
+        ArrayList<Path> result = new ArrayList<>();
+
+        Path p = Path.of("..", OUTPUT_DIRECTORY,username, name);
+
+        if (Files.exists(p)) {
+            try (Stream<Path> stream = Files.walk(p)) {
+                result = (ArrayList<Path>) stream.filter(x -> x.toFile().isFile()).collect(Collectors.toList());
+            }
+        }
+
+        return result;
+    }
+
+    public static boolean createDirectory(String username, String name) throws IOException {
+        if(username.isEmpty()) throw new IllegalArgumentException(USERNAME_WARNING);
+        if(name.isEmpty()) throw new IllegalArgumentException(PATH_WARNING);
+
+        boolean result = false;
+
+        Path p = Path.of("..", OUTPUT_DIRECTORY,username, name);
+
+        if (!Files.exists(p)) {
+            Files.createDirectory(p);
+            result = true;
+        }
+        return result;
     }
 }
---
+++ project/common/src/test/java/tests/FileHelperTest.java
@@ -0,0 +1,44 @@
+package tests;
+
+import helpers.FileHelper;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileHelperTest {
+
+    @org.junit.jupiter.api.Test
+    void saveFile() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getFile() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void listDirectories() throws IOException {
+        ArrayList<Path> r = (ArrayList<Path>) FileHelper.listDirectories("test");
+
+        for (Path p: r) {
+            System.out.println(p.getFileName());
+        }
+    }
+
+    @org.junit.jupiter.api.Test
+    void listFilesInDirectory() throws IOException {
+        ArrayList<Path> r = (ArrayList<Path>) FileHelper.listFilesInDirectory("test", "test2");
+
+        for (Path p: r) {
+            System.out.println(p.getFileName());
+        }
+    }
+
+    @org.junit.jupiter.api.Test
+    void createDirectory() throws IOException {
+        assertTrue(FileHelper.createDirectory("test", "test2"));
+    }
+}
